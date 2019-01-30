62a82575d35eef2306539bdadc6793ef4281e750
BTH <BTH@H473-0013.students.ad.bth.se>
2019-01-30 08:56:59 +0100

    Ref added

---
+++ Leaf-Drop/Source/Core.cpp
@@ -0,0 +1,11 @@
+#include "Core.h"
+
+Core::Core()
+{
+	std::cout << "lol" << std::endl;
+}
+
+
+Core::~Core()
+{
+}
---
+++ Leaf-Drop/Source/Core.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+
+class Core
+{
+public:
+	Core();
+	~Core();
+
+
+};
+
